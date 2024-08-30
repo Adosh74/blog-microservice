6d1bf65982b7785c14d84843770b98f3e64e372e
Mohamed Shebl <hamdysheblm@gmail.com>
2024-08-30 18:35:15 +0300

    adding data to event bust when create comment add events route

--- comments/index.js
+++ comments/index.js
@@ -1,13 +1,16 @@
 import express  from 'express';
 import { randomBytes } from 'crypto';
+import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const commentByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-    const comments = commentByPostId[req.body.id] || []
+    const comments = commentByPostId[req.params.id] || []
 
     res.status(200).json({
         success: true,
@@ -16,27 +19,51 @@ app.get('/posts/:id/comments', (req, res) => {
     })
 });
 
-app.post('/posts/:id/comments', (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
+app.post('/posts/:id/comments', async(req, res) => {
+    try {
+        const commentId = randomBytes(4).toString('hex');
 
-    const { content } = req.body; 
+        const { content } = req.body; 
 
-    const comments = commentByPostId[req.params.id] ?
-                    commentByPostId[req.params.id].push({
-                        id: commentId,
-                        content
-                    }):
-                    commentByPostId[req.params.id] = [{
-                        id: commentId,
-                        content
-                    }]
+        const comments = commentByPostId[req.params.id] ?
+                        commentByPostId[req.params.id].push({
+                            id: commentId,
+                            content
+                        }):
+                        commentByPostId[req.params.id] = [{
+                            id: commentId,
+                            content
+                        }]
 
-    res.status(200).json({
-        success: true,
-        result: comments.length, 
-        data: comments
-    })
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentCreated',
+            data: {
+                id: commentId,
+                content,
+                postId: req.params.id
+            }
+        }).catch((error) => {
+            throw error;
+        });
+
+        res.status(201).json({
+            success: true,
+            result: comments.length, 
+            data: [comments]
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        })
+    }
 });
+
+app.post('/events', (req, res) => {
+    console.log('Received Event:', req.body.type);
+    res.sendStatus(200);
+})
 
 app.listen(4001, () => {
     console.log(`comments service running on port: ${4001}`)
