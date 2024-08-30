839001c7d9742a72e0d3980c51086ae060b36ec7
Mohamed Shebl <hamdysheblm@gmail.com>
2024-08-30 18:35:35 +0300

    adding data to event bus when create post add events route

--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,14 @@
 import express from  'express'
 import { randomBytes } from 'crypto'
+import cors from 'cors'
+import axios from 'axios'
 
 const app = express();
 
 const posts = {}
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/posts', (req, res) => {
     res.status(200).json({
@@ -14,19 +17,44 @@ app.get('/posts', (req, res) => {
     })
 })
 
-app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
+app.post('/posts', async(req, res) => {
+    try {
+        const id = randomBytes(4).toString('hex');
 
-    const {title} = req.body;
-    posts[id] = {
-        id, 
-        title
+        const {title} = req.body;
+        posts[id] = {
+            id, 
+            title
+        }
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'PostCreated',
+            data: {
+                id, 
+                title
+            }
+        }).catch((error) => {
+            throw error;
+        });
+
+        res.status(201).json({
+            success: true,
+            data: posts[id]
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        })
+        
     }
+    
+})
 
-    res.status(201).json({
-        success: true,
-        data: posts[id]
-    })
+app.post('/events', (req, res) => {
+    console.log('Received Event:', req.body.type);
+    res.sendStatus(200);
 })
 
 app.listen(4000, () => {
