3709b8ee4156aae5f461491a93a46e5f4d7d50a4
Mohamed Shebl <hamdysheblm@gmail.com>
2024-08-31 19:05:53 +0300

    store events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,14 +5,19 @@ const app = express();
 
 app.use(express.json());
 
+const events = []
+
 
 app.post('/events', async (req, res) => {
     try {
         const event = req.body;
 
+        events.push(event)
+        
         await axios.post('http://localhost:4000/events', event);
         await axios.post('http://localhost:4001/events', event);
         await axios.post('http://localhost:4002/events', event);
+        await axios.post('http://localhost:4003/events', event);
 
         res.sendStatus(200);
     } catch (error) {
@@ -20,6 +25,13 @@ app.post('/events', async (req, res) => {
         res.sendStatus(500);
     }
 });
+
+app.get('/events', (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: events
+    })
+})
 
 app.listen(4005, () => {
     console.log('Event bus listening on port 4005');
